544f2e71cd89b84b115bfcf0902ddfe16b4e4e90
Yasuhiro Osajima <kimukimu33@gmail.com>
2023-12-31 00:15:20 +0900

    update table page

--- src/app/table/page.tsx
+++ src/app/table/page.tsx
@@ -2,9 +2,11 @@
 
 import { useEffect } from "react";
 import { useRecoilState } from "recoil";
+import { Box, Card, CardContent, Grid, Typography } from "@mui/material";
 
 import { HeaderTitleAtom } from "@/state/HeaderTitleAtom";
 import Table from "@/components/tablePage/Table";
+import MessageArea from "@/components/tablePage/MessageArea";
 
 export default function TablePage() {
   const [_, setTitle] = useRecoilState(HeaderTitleAtom);
@@ -13,5 +15,29 @@ export default function TablePage() {
     setTitle("Table");
   }, [setTitle]);
 
-  return <Table />;
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Card sx={{ height: "100%" }}>
+            <CardContent>
+              <Typography variant="h6" component="div">
+                Person table
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={8}>
+          <Card sx={{ height: "100%" }}>
+            <Table />
+          </Card>
+        </Grid>
+        <Grid item xs={4}>
+          <Card sx={{ height: "100%" }}>
+            <MessageArea />
+          </Card>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
--- src/components/tablePage/MessageArea.tsx
+++ src/components/tablePage/MessageArea.tsx
@@ -1,9 +1,22 @@
 "use client";
 
+import { Card, CardContent, Typography } from "@mui/material";
+
 export default function MessageArea() {
   return (
-    <div>
-      <p>MessageArea</p>
-    </div>
+    <Card sx={{ height: "100%" }}>
+      <CardContent>
+        <Typography variant="h6" component="div">
+          MessageArea
+        </Typography>
+        <br />
+
+        <Typography variant="body2">
+          hogehoge
+          <br />
+          fugafuga
+        </Typography>
+      </CardContent>
+    </Card>
   );
 }
